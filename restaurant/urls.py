25d788952506b0e7ee1feaec126e4918741384e9
Sivu <49073182+sivay1@users.noreply.github.com>
2024-01-09 11:19:15 +0530

    Delete restaurant directory

--- restaurant/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from restaurant import views
-
-urlpatterns = [
-		path("book/",views.BookingView.as_view()),
-		path("menu/",views.MenuView.as_view(),name='menu-view'),
-		
-]
